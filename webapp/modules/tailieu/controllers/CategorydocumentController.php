f724430280760a8262102280c6cbc15c571f2a57
tanpd <phamduytan2612@gmail.com>
2019-09-10 02:02:36 +0700

    fix search

--- webapp/modules/tailieu/controllers/CategorydocumentController.php
+++ webapp/modules/tailieu/controllers/CategorydocumentController.php
@@ -45,7 +45,6 @@ class CategorydocumentController extends TailieuController {
             $outputs = $this->_showDocument($array['id'], 0);
             $output = $this->_showCategoryLV2($array['id']);
         }
-        var_dump($array);die();
 
         $this->render('index', array(
             'data' => $array,
