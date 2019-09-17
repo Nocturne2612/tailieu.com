1cac7d5e9080470f7274456f2a93ef377482200c
Hoang Pham <p.hoang121296@gmail.com>
2019-09-17 18:32:54 +0700

    fix UI

--- webapp/controllers/ApiController.php
+++ webapp/controllers/ApiController.php
@@ -14,7 +14,7 @@ class ApiController extends CController {
         $data = array(
             
         );
-        $info = Yii::app()->user->getState('tAccount');
+        $info = Yii::app()->user->getState();
         if (!$info) {
             $error_code = 1;
         } else {
